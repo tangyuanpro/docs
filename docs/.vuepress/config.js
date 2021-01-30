a235ef5200ad54a94fd3c0e8527e8aa5e0b153e8
tangyuanpro <46566057+tangyuanpro@users.noreply.github.com>
2021-01-30 21:01:51 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme:"antdocs",
   title: "Yexi Docs",
   description: "好教程就在夜夕文库",
-  base: "/docs/",
+  base: "/yxdocs/",
   head: [
     ["link",{ rel: "icon",href: "/assets/logo.png" }]
   ],
