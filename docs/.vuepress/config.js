edc9b627b6fd236e8c25e93218c39da432ecec00
tangyuanpro <46566057+tangyuanpro@users.noreply.github.com>
2021-01-31 15:01:33 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,17 +23,17 @@ module.exports = {
             }
           }
         },
-  locales: {	
-    '/': {	
-      lang: 'zh-Hans',	
-      description: '技术 生活 二次元'	
-    }	
-  },
     smoothScroll: true,
     nav: require("./config/nav"),
     sidebar: require("./config/sidebar"),
     lastUpdated: "最后一次编辑时间",
     repo: "https://github.com/tangyuanpro",
     editLinks: false,
+  },
+  locales: {	
+    '/': {	
+      lang: 'zh-Hans',	
+      description: '技术 生活 二次元'	
+    }	
   },
 };
